be503e3c66c55f2d0314b4603a1471f709de36db
白哥 <1069121849@qq.com>
2020-07-29 20:26:46 +0800

    创建机器人

--- app/controller/home.js
+++ app/controller/home.js
@@ -2,10 +2,28 @@
 
 const Controller = require('egg').Controller;
 
+const {
+  Wechaty,
+} = require('wechaty');
+
+const bot = new Wechaty({
+  name: 'myWechatyBot',
+});
 class HomeController extends Controller {
   async index() {
     const { ctx } = this;
+    bot.on('login', this.onLogin);
+    bot.start()
+      .catch(async e => {
+        console.error('Bot start() fail:', e);
+        await bot.stop();
+        // process.exit(-1);
+      });
     ctx.body = 'hi, egg';
+  }
+  onLogin(user) {
+    console.log(`${user.name()} login`);
+    bot.say('Wechaty login').catch(console.error);
   }
 }
 
