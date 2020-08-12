b12b06959d0baea46cb4d7ccd20ff5e30eaab8c3
白哥 <1069121849@qq.com>
2020-08-12 21:52:28 +0800

    创建机器人

--- app/extend/application.js
+++ app/extend/application.js
@@ -2,7 +2,7 @@
 
 const constance = require('../constance');
 const WebSocket = require('ws');
-const ws = new WebSocket('ws://127.0.0.1:5555');
+const ws = new WebSocket('ws://127.0.0.1:5555', '', {});
 
 module.exports = {
   get constance() {
--- app/model/user.js
+++ app/model/user.js
@@ -3,30 +3,13 @@ module.exports = app => {
   const mongoose = app.mongoose;
   const Schema = mongoose.Schema;
   const UserSchema = new Schema({
-    wxId: {
+    wxid: {
       type: String,
     },
-    username: {
+    name: {
       type: String,
-    },
-    nickname: {
-      type: String,
-      default: '',
     },
     createTime: { type: Date, default: Date.now },
-    email: {
-      type: String,
-      default: '',
-    },
-    sex: {
-      type: Number,
-      default: 0,
-    },
-    intro: {
-      type: String,
-      default: '',
-    },
-    age: { type: Number, min: 0, max: 120, default: 0 },
   });
   return mongoose.model('User', UserSchema, 'User');
 };
